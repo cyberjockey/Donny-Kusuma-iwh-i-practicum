f709ef37f516c3deb8981f78c8d6cd334b071beb
cyberjockey <50918643+cyberjockey@users.noreply.github.com>
2024-11-06 12:16:25 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ const axios = require('axios');
 const app = express();
 
 app.set('view engine', 'pug');
-app.use(express.static(__dirname + '/public'));  // Serving static files from 'public' folder
-app.use(express.urlencoded({ extended: true })); // Parsing URL-encoded data
-app.use(express.json());  // Parsing JSON data
+app.use(express.static(__dirname + '/public')); 
+app.use(express.urlencoded({ extended: true })); 
+app.use(express.json());  
 
-// Please DO NOT INCLUDE the private app access token in your repo. Use environment variables for security.
-const PRIVATE_APP_ACCESS = process.env.HUBSPOT_PRIVATE_APP_ACCESS_TOKEN || ''; // Use an env variable instead
+
+const PRIVATE_APP_ACCESS = process.env.HUBSPOT_PRIVATE_APP_ACCESS_TOKEN || '';
 
 // ROUTE 1 - Fetch custom object data and render homepage
 app.get('/', async (req, res) => {
-    const customObjectsUrl = 'https://api.hubapi.com/crm/v3/objects/custom_objects'; // URL to fetch custom objects
+    const customObjectsUrl = 'https://api.hubapi.com/crm/v3/objects/2-36517265'; // URL to fetch custom objects
     const headers = {
         Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
         'Content-Type': 'application/json'
@@ -40,13 +40,13 @@ app.get('/update-cobj', (req, res) => {
 app.post('/update-cobj', async (req, res) => {
     const newRecord = {
         properties: {
-            property1: req.body.property1, // Custom field 1
-            property2: req.body.property2, // Custom field 2
-            property3: req.body.property3  // Custom field 3
+            property1: req.body.dino_name, // Custom field 1
+            property2: req.body.dino_paddock, // Custom field 2
+            property3: req.body.dino_dietary_type  // Custom field 3
         }
     };
     
-    const customObjectUrl = 'https://api.hubapi.com/crm/v3/objects/custom_objects'; // API URL for custom objects
+    const customObjectUrl = 'https://api.hubapi.com/crm/v3/objects/2-36517265'; // API URL for custom objects
     const headers = {
         Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
         'Content-Type': 'application/json'
